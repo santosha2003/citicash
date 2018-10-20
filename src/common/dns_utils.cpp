51b29fead91d403d6e4a08633903fbdecd160c54
santosha2003 <36627236+santosha2003@users.noreply.github.com>
2018-10-20 22:09:58 +0300

    Update dns_utils.cpp

--- src/common/dns_utils.cpp
+++ src/common/dns_utils.cpp
@@ -91,7 +91,7 @@ static const char*
 get_builtin_ds(void)
 {
   return
-". IN DS 20326 8 2 E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC683457104237C7F8EC8D\n"";
+". IN DS 20326 8 2 E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC683457104237C7F8EC8D\n";
 }
 
 /************************************************************
